3bdd19627734c76afc2bb6cc1ddd8f4a30db2bb2
Aouf Ali <40uf4l1@gmail.com>
2019-12-06 14:43:53 +0100

    adding quadruplets classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+//        CharStream charStream = CharStreams.fromFileName("./code.txt");
+//        TlLexer lexer = new TlLexer(charStream);
+//        TlParser parser = new TlParser(new CommonTokenStream(lexer));
+//        ParseTree tree = parser.start();
+//        Integer answer = new MyVisitor().visit(tree);
+//        System.out.printf("%s = %s\n", "the result is: ", answer);
     }
 }
---
+++ src/QuadGenerator.java
@@ -0,0 +1,25 @@
+
+
+public class QuadGenerator extends TlBaseVisitor<Integer> {
+
+    private Quads quads = new Quads();
+
+    private void showText(String text, int typeOfText)
+    {
+        TextDisplayer.getInstance().showText(text,typeOfText,TextDisplayer.QUADGEN);
+    }
+
+    @Override
+    public Integer visitStart(TlParser.StartContext ctx) {
+
+        quads.addQuad("END","","","");
+        showText("generated quads: ",TextDisplayer.COMPILERTEXTS);
+        showText("******************************************************",TextDisplayer.COMPILERTEXTS);
+        for (int i = 0; i < quads.size(); i++) {
+            showText(i + ": " + quads.getQuad(i).toString(),TextDisplayer.COMPILERTEXTS);
+        }
+        showText("******************************************************",TextDisplayer.COMPILERTEXTS);
+
+        return visitChildren(ctx);
+    }
+}
